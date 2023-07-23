d5a9741c10b5d5084028a034cfb7b2dbb42e0b0a
Arjun Mitra <103885618+Arjun0055@users.noreply.github.com>
2023-07-23 20:43:41 +0530

    Update stopwatch.js
    Added functioning for the hours time in the stopwatch. Added sound effects for hours.
    
    i.e., Every hour the stopwatch hits it will make a sound.

--- stopwatch.js
+++ stopwatch.js
@@ -1,16 +1,26 @@
-let seconds = 00;
-let micro_seconds = 00;
-let minutes = 00;
+let seconds = 0o0;
+let micro_seconds = 0o0;
+let minutes = 0o0;
+let hours = 0o0;
+
+function playMusic() {
+
+    let audio = new Audio("space-alarm.mp3");
+    audio.play()
+}
+
 
 let getSeconds = document.querySelector('.seconds');
 let getmicro_seconds = document.querySelector('.micro_seconds');
 let getMinutes = document.querySelector('.minutes');
+let getHours = document.querySelector('.hours');
 
 let btnStart = document.querySelector('.btn-start');
 let btnStop = document.querySelector('.btn-stop');
 let btnReset = document.querySelector('.btn-reset');
 
 let interval;
+let n=9;
 
 btnStart.addEventListener('click', ()=>{
     
@@ -28,6 +38,8 @@ btnReset.addEventListener('click', ()=>{
     micro_seconds = '00';
     seconds = '00';
     minutes = '00';
+    hours = '00';
+    getHours.innerHTML = hours;
     getMinutes.innerHTML = minutes;
     getSeconds.innerHTML = seconds;
     getmicro_seconds.innerHTML = micro_seconds;
@@ -52,14 +64,25 @@ function startTimer(){
     if(seconds > 9){
         getSeconds.innerHTML = seconds;
     }
-    if(seconds > 60){
+    if(seconds > 59){
         minutes++;
         getMinutes.innerHTML = '0' + minutes;
         seconds = 0;
         getSeconds.innerHTML = '0' + 0;
     }
-    if(minutes >= 99){
+    if(minutes > 9){
+        getMinutes.innerHTML = minutes;
+        
+    }
+    if(minutes > 59){
+
+        hours++;
+        playMusic();
+        getHours.innerHTML = '0' + hours;
+        minutes = 0;
+        getMinutes.innerHTML = '0' + 0;
+    }
+    if(hours >=99){
         clearInterval;
     }
-    
 }
